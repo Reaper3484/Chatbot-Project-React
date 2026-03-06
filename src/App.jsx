859d193b64bd5fe346e940d5767df0c6e3fed480
Arnav Kumar <kmr.arnav3484@gmail.com>
2026-03-07 00:39:08 +0530

    Added local storage for chatmessages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ChatMessages from './components/ChatMessages'
 import { ChatInput } from './components/ChatInput'
 import './App.css'
 
 
 function App() {
-  const [chatMessages, setChatMessages] = useState([])
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || [])
   const [showScrollButton, setShowScrollButton] = useState(false)
+
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages))
+  }, [chatMessages])
 
   return (
     <div className="app-container">
-      {chatMessages.length == 0
+      {chatMessages.length === 0
         ? <p className="welcome-text">Welcome to the Chatbot project! Send a message by using the textbox below!</p>
         : null}
 
       <ChatMessages chatMessages={chatMessages}
         showScrollButton={showScrollButton}
         setShowScrollButton={setShowScrollButton} />
+
       <ChatInput setChatMessages={setChatMessages}
         showScrollButton={showScrollButton}
         setShowScrollButton={setShowScrollButton} />
